cb2f5f6d849c0eefa3557e021d9c38937ace1ac8
morlinbrot <morlinbrot@mailbox.org>
2023-10-29 12:22:05 +0100

    Add Typescript + ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+    "prettier" // This disables rules in presets that conflict with prettier (or other formatters).
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true, // Find the closest tsconfig.json for each source file.
+    tsconfigRootDir: __dirname,
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+}
